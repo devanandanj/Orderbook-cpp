85d478cd333334eef4a1063e5828e487c426697e
Devanandanj <jibudevanandan@gmail.com>
2026-08-04 21:17:57 +0530

    basic blocks done.

---
+++ include/using.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+
+using Price = std::int32_t;
+using OrderId = std::uint64_t;
+using Quantity = std::uint32_t;
+
--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,10 +1,55 @@
 ﻿
+#include <iostream>
+#include <map>
+#include <list>
+#include "../include/using.h"
 
-#include "../include/orderbook.h"
+
+enum class Side : uint8_t {
+	Buy,
+	Sell
+};
+
+
+struct LevelInfos {
+	OrderId orderid_;
+	Quantity quantity_;
+};
+
+
+struct Order {
+	OrderId orderId;
+	Side side;
+	Price price;
+	Quantity quantity;
+
+};
+
+struct OrderModify{
+	OrderId orderId_;
+	Side side_;
+	Price price_;
+	Quantity quantity_;		//Execure(E) not implemented, no partial fill tracking.
+};
+
+Order MakeOrder(OrderId orderId, Side side, Price price, Quantity quantity) {
+	return Order{ orderId, side, price, quantity };
+}
+
+OrderModify MakeOrderModify(OrderId orderId, Side side, Price price, Quantity quantity) {
+	return OrderModify{ orderId, side, price, quantity };
+}
+
+bool isBuyOrder(const Order& order) {
+	return order.side == Side::Buy;
+}
 
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	Order o = MakeOrder(1, Side::Buy, 100, 10);
+
+	std::cout << "Order" << o.orderId << "Price" << o.price << "Quantity" << o.quantity << std::endl;
+	
 	return 0;
 }
